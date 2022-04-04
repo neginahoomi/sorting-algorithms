2faf7efc656b20b7d0ee9653dd4a020c8ad0ee34
Negin <17837645+neginahoomi@users.noreply.github.com>
2022-04-04 12:29:00 -0400

    added insertion sort

---
+++ insertion_sort.py
@@ -0,0 +1,9 @@
+def insertion_sort(array):
+    for j in range(0, len(array)):
+        key = array[j]
+        i = j-1
+        while i>=0 and array[i]>key:
+            array[i+1] = array[i]
+            i = i -1
+        array[i+1] = key
+    return array
